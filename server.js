6da33e22d0838380cc17300b69ccc730944209c5
Abdualla Ibrahim Al-Gouda <abduallah.ibrahim.gouda@gmail.com>
2021-02-16 21:06:22 +0200

    last

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const bodyParser = require("body-parser");
 const app = express();
 
 
-const headtitle = "Creative Writing";
-const body_text = "Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph into their writing."
+const headtitle = [];
+const body_text = [];
+if(headtitle.length = 0){
+    headtitle
+}
 
 app.set("view-engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}))
@@ -15,13 +18,38 @@ app.use(express.static("public"))
 app.get('/', function (req, res) {
     res.render("home.ejs",{
         headtitle : headtitle ,
-        body_text : body_text
+        body_text : body_text 
+    })
+})
+
+app.get('/about', function (req, res) {
+    res.render("about.ejs",{
+
+    })
+})
+
+app.get('/contact', function (req, res) {
+    res.render("contact.ejs",{
+
     })
 })
 
 
-app.post('/', function (req, res) {
+app.get('/compose', function (req, res) {
+    res.render("compose.ejs",{
 
+    })
+})
+
+
+app.post('/compose', function (req, res) {
+    console.log(req.body.data)
+    body_text.push(req.body.data) ;
+    headtitle.push(req.body.title) ;
+    res.render("home.ejs",{
+        headtitle : headtitle ,
+        body_text : body_text
+    })
 })
 
 
